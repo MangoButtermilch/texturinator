17e7577bf1509e3abe55ac9aa62e878c1eb45467
MangoButtermilch <aceofbeats883@gmail.com>
2026-05-13 15:52:51 +0200

    add settings service for local/session storage

---
+++ src/app/shared/services/settings.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+
+  constructor() { }
+
+  public setLocalSetting(key: string, value: string): void {
+    localStorage.setItem(key, value);
+  }
+
+  public getLocalSetting(key: string): string | null {
+    return localStorage.getItem(key);
+  }
+
+  public saveSessionSetting(key: string, value: string): void {
+    sessionStorage.setItem(key, value);
+  }
+
+  public getSessionSetting(key: string): string | null {
+    return sessionStorage.getItem(key);
+  }
+
+}
